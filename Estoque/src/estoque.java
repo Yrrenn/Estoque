8cad50bc48afec65c603e86e679fb5351875ac68
Yrrenn <akiradaeo96@gmail.com>
2026-06-17 21:25:35 -0400

    Loop + Array
    add while, agora o sistema ta em loop e ad array list pra add os produtos, futuramente vou ter que add um array para a quantidade

--- Estoque/src/estoque.java
+++ Estoque/src/estoque.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;   //imorta o scanner
-import java.ultil.ArrayList; // importa a classe ArrayList
+import java.util.ArrayList; // importa a classe ArrayList
+
 
 public class estoque {  //declara a classe estoque 
 
@@ -7,32 +8,40 @@ public class estoque {  //declara a classe estoque
 
     public static void main(String[] args) throws Exception { // declara o metodo main
 
-     
-        Scanner teclado = new Scanner(System.in); // cria um objeto scanner
-         ArrayList<String> descricao = new ArrayList<String>(); // // Cria uma Lista que armazena os produtos (Array e um vetor multidimensional que aloca espacos na memoria)
-     
+        Scanner teclado = new Scanner(System.in);  // cria um objeto scanner
+        
+
+        ArrayList<String> descricao = new ArrayList<String>();   // Cria uma Lista que armazena os produtos (Array e um vetor multidimensional que aloca espacos na memoria)
+                
+
+        int opcao = -1; // inicializa a variavel opcao com -1, para que o programa entre no loop while, caso escolha 0, o programa termina.
+
+        while (opcao !=0) { // enquanto a opcao for diferente de 0, o programa continua rodando, e o usuario pode escolher o que deseja fazer, caso escolha 0, o programa termina.
+
         System.out.println("---Bem Vindo ao Controle de Estoque---");        
         System.out.println("1.Cadastrar Produto");                              
         System.out.println("2.Listar Produtos");                         // menu de navegacao
-        System.out.println("3.Excluir Produto");                             
+        System.out.println("3.Atualizar Estoque");                      
         System.out.println("0.Sair");                                   
+        System.out.println("-------------------------------------");
+        System.out.println("Escolha uma opção:"); // pede para o usuario escolher uma opcao
+        opcao = teclado.nextInt(); // le a opcao escolhida pelo usuario
 
 
-        int opcao = teclado.nextInt();  // essa variavel recebe a opcao do usuario, usado para navegacao no menu, e a principal fucao do programa, pois a partir dela o usuario pode escolher o que deseja fazer. seu feio!
 
-        
+
         switch (opcao) {
+            
             case 1:
                 System.out.println("--Cadastrar Produto--");
-                System.out.println("Produto:");
-                teclado.nextLine();                                             //Eu tenho que fazer ele voltar ao inicio do sistema, pra poder listar os produtos
-                                                                                //Ta foda, to com dificuldade em array list e tenha que lembrar com usa o while
+                System.out.println("Nome do produto:");
+                teclado.nextLine();
 
-                String produto = teclado.nextLine();                                    
+                String produto = teclado.nextLine();
                 descricao.add(produto); 
             
-                System.out.println("Produto cadastrado com sucesso!"); 
-              
+                System.out.println(produto + " cadastrado com sucesso!");
+                
 
 
                 // Lógica para cadastrar produto
@@ -40,12 +49,14 @@ public class estoque {  //declara a classe estoque
             case 2:
                 System.out.println("Listar Produtos");
 
-
-
+                
+            
+        
                 // Lógica para listar produtos
                 break;
             case 3:
                 System.out.println("Excluir Produto");
+
 
 
 
@@ -59,7 +70,8 @@ public class estoque {  //declara a classe estoque
             default:
                 System.out.println("Opção inválida. Tente novamente.");
         }
-
+    }
+    
         teclado.close();
     }
 }
